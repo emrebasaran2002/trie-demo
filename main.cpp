5b5b6bcd783137f23753c226e4842bca671cfb36
emrebasaran2002 <emrebasaran2002>
2024-07-28 13:56:53 -0400

    Started writing tests

--- main.cpp
+++ main.cpp
@@ -1,25 +1,70 @@
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <cassert>
 #include "Trie.hpp"
 
-int main(void) {
+struct Test final {
+    std::string name;
+    void (*func)();
+
+    Test(const std::string& name, void (*func)()): name{name}, func{func} {}
+};
+
+// Helper for verifying the contents of a trie, using the << operator.
+void checkContents(const Trie& trie, const std::string& expected) {
+    std::ostringstream stream;
+    stream << trie;
+    const std::string actual = stream.str();
+    assert(actual == expected);
+}
+
+
+void emptyTrieTest() {
     Trie trie;
+    checkContents(trie, "[]");
+}
 
-    trie.add("abc");
-    trie.add("abd");
-    std::cout << trie << std::endl;
+void copyAssignmentOperator() {
+    Trie trie1;
+    Trie trie2;
 
-    std::cout << "a: " << trie.contains("a") << std::endl;
-    std::cout << "b: " << trie.contains("b") << std::endl;
-    std::cout << "ab: " << trie.contains("ab") << std::endl;
-    std::cout << "abc: " << trie.contains("abc") << std::endl;
-    std::cout << "abd: " << trie.contains("abd") << std::endl;
+    trie1.add("hello");
+    trie1.add("world");
+    trie1 = trie2;
+    checkContents(trie1, "[]");
+    checkContents(trie2, "[]");
 
-    trie.remove("abc");
-    std::cout << trie << std::endl;
+    trie1.add("emre");
+    trie2.add("duru");
+    trie1 = trie2;
+    checkContents(trie1, "[\"duru\"]");
+    checkContents(trie2, "[\"duru\"]");
 
-    trie.remove("abd");
-    std::cout << trie << std::endl;
+    trie1.remove("duru");
+    trie2.add("emre");
+    trie1 = trie2;
+    checkContents(trie1, "[\"duru\", \"emre\"]");
+    checkContents(trie2, "[\"duru\", \"emre\"]");
 
-    std::cout << "Done!" << std::endl;
+    trie1.remove("duru");
+    trie1.remove("emre");
+    trie1.add("abc");
+    trie1 = trie1;
+    checkContents(trie1, "[\"abc\"]");   
+    checkContents(trie2, "[\"duru\", \"emre\"]");
+}
+
+int main(void) {
+    std::vector<Test> tests;
+    tests.emplace_back("emptyTrieTest", &emptyTrieTest);
+    tests.emplace_back("copyAssignmentOperator", &copyAssignmentOperator);
+    
+    for (const Test& test : tests) {
+        std::cout << "Running: " << test.name << std::endl;
+        test.func();
+    }
+
+    std::cout << "All tests pass. :)" << std::endl;
     return 0;
 }
